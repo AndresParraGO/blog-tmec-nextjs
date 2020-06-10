33510bd10a70a1ca321036ebfc0cdcf7510b1a23
AndresParraGO <andresparragn@gmail.com>
2020-06-10 08:38:55 -0500

    Create the components lasts posts anc card

--- components/lasts-posts.js
+++ components/lasts-posts.js
@@ -4,36 +4,70 @@ import Link from 'next/link'
 
 import getData from '../services/api.js'
 
+import Card from './card.js'
+
 
 function LastsPosts() {
   const [posts, setPosts] = useState([])
 
+  async function data() {
+    const dataFetch = await getData()
+    setPosts(dataFetch.data.posts.reverse())
+  }
+
   useEffect(() => {
-    fetch('https://us-central1-tmec-api.cloudfunctions.net/api')
-      .then(res => res.json())
-      .then(data => setPosts(data.data.posts))
-      .catch(() => {
-          setPosts({
-              done: true,
-              success: false
-          })
-        })
+    data()
   }, [])
 
-  if(posts === []) {
+  if(!posts) {
     return <span>Loading ...</span>
   }
 
+  let i = 0
 
-  return(
-    <div className="container">
-      {
-        posts.map(({title, id}) => {
-          return <h1 key={id}>{title}</h1>
-        })
-      }
+  return (
+    <div className="posts container">
+      <h1>Ultimos Posts</h1>
+      <section className="lasts-posts">
+        {
+          posts.map(({ title, id, description, img, path }) => {
+            i++
+            if(i <= 3) {
+              return (
+                <Card
+                  title={title}
+                  key={id}
+                  description={description}
+                  img={img}
+                  path={path}
+                />
+              )
+            }
+          })
+        }
+      </section>
+
+      <style jsx>{`
+        .posts {
+          margin-top:4em;
+          margin-bottom:4em;
+        }
+
+        .lasts-posts {
+          display: grid;
+          grid-gap: 2em;
+        }
+
+        @media screen and (max-width: 650px) {
+          grid-template-columns: repeat(1, 1fr);
+        }
+
+        @media screen and (min-width: 960px) {
+          grid-template-columns: repeat(3, 1fr);
+        }
+      `}</style>
     </div>
-  )
+  );
 }
 
 
